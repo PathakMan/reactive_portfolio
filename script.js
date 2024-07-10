50930bf03459e610472201ed8576861f3b27e6bb
Manthan Pathak <100357753+PathakMan@users.noreply.github.com>
2024-07-10 11:17:49 +0200

    new update

--- script.js
+++ script.js
@@ -5,10 +5,29 @@ function toggleMenu() {
     icon.classList.toggle("open")
 }
 
-function playVideo(e) {
-    e.play();
-    e.classList.remove('fading');
-    setTimeout(() => {
-      e.classList.add('fading');
-    }, (e.duration / e.playbackRate - 1) * 1000);
+
+const nameElement = document.getElementById('name');
+const name = 'Manthan Pathak';
+let i = 0;
+
+function nameTitle() {
+  if (i < name.length) {
+    nameElement.textContent += name[i];
+    i++;
+    setTimeout(nameTitle, 100); // Simulate typing speed
+  } else {
+    setTimeout(erase, 2000); // Wait 2 seconds before erasing
   }
+}
+
+function erase() {
+  if (i > 0) {
+    nameElement.textContent = nameElement.textContent.substring(0, i - 1);
+    i--;
+    setTimeout(erase, 50); // Simulate erasing speed
+  } else {
+    setTimeout(nameTitle, 1000); // Wait 1 second before typing again
+  }
+}
+
+nameTitle(); // Start the animation
